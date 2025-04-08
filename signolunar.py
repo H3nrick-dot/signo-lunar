6efacd39d712d2c5d58695c250d8597fdd5b88ee
H3nrick-dot <Henriquecesar810@gmail.com>
2025-04-08 16:43:06 -0300

    Update signolunar.py

--- signolunar.py
+++ signolunar.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import swisseph as swe
 import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -31,4 +32,5 @@ def lunar_sign():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
